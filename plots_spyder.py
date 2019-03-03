1f18db7d9533efafd4a91cf20c4ae0c8699d09a2
ZosiaStafford <46976875+ZosiaStafford@users.noreply.github.com>
2019-03-03 18:21:35 -0500

    Update plots_spyder.py

--- plots_spyder.py
+++ plots_spyder.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math as m
+    
 
 def open_file(filename):
     fin = open(filename)
     new = open("data_plots.py", 'w+')
     
-    t = []
+    a = []
     
     for line in fin:
         count = 0
@@ -13,4 +15,13 @@ def open_file(filename):
         word = line.strip()
         
         delimeter = ","
-        t = word.split(delimeter)
+        a = word.split(delimeter)
+        
+        t = a[0]
+        x = a[1]
+        y = a[2]
+        z = a[3]
+        ang = m.atan2(x,(math.sqrt(y**2+z**2)))
+        vel =
+        acc =
+
