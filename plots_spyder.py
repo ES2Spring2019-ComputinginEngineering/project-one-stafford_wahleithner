568414d270f09c13f9a8e81d5f49ce48e8a28c75
ZosiaStafford <46976875+ZosiaStafford@users.noreply.github.com>
2019-03-03 19:00:41 -0500

    Update plots_spyder.py

--- plots_spyder.py
+++ plots_spyder.py
@@ -1,27 +1,81 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import math as m
-    
+#check: confirm equations, how to add plots to file (+ need file?), 
+#how to get velocity/acceleration graphs, format plots (period), report, due date time!!
 
-def open_file(filename):
-    fin = open(filename)
-    new = open("data_plots.py", 'w+')
+import matplotlib.pyplot as plt
+import math as m
+import os
+cwd = os.getcwd()
+os.chdir("C:\\Users\zosia\mu_code")
+
+filename = input("What is the filename? ")
+fin = open(filename)
+new = open("data_plots.py", 'w+')
+
+a = []
+ang_list = []
+vel_list = []
+acc_list = []
+t_list = []
+
+for line in fin:
+    word = line.strip()
     
-    a = []
+    delimeter = ","
+    a = word.split(delimeter)
     
-    for line in fin:
-        count = 0
-        i = 0
-        word = line.strip()
-        
-        delimeter = ","
-        a = word.split(delimeter)
-        
-        t = a[0]
-        x = a[1]
-        y = a[2]
-        z = a[3]
-        ang = m.atan2(x,(math.sqrt(y**2+z**2)))
-        vel =
-        acc =
+    t = float(a[0])
+    x = float(a[1])
+    y = float(a[2])
+    z = float(a[3])
+    
+    ang = m.atan2(x,(m.sqrt(y**2+z**2)))
+    vel = ang/t
+    acc = -9.81/(15*m.sin(ang))
+    
+    ang_list.append(ang)
+    vel_list.append(vel)
+    acc_list.append(acc)
+    t_list.append(t)
+
+print(ang_list)
+print("****")
+print(vel_list)
+print("****")
+print(acc_list)
+print("****")
+print(t_list)
+
+plot = plt.figure(figsize = (4,6))
+
+plt.subplot(3,1,1)
+plt.plot(t_list, ang_list, 'r-') 
+plt.xlabel('Time (seconds)')
+plt.ylabel('Angle (rad)')
+plt.title('Angle vs Time')
+plt.xlim((0, a[0]))
+plt.grid()
+
+
+plt.subplot(3,1,2)
+plt.plot(t_list, vel_list, 'b-') 
+plt.xlabel('Time (seconds)')
+plt.ylabel('Angular Velocity (rad/s)')
+plt.title('Angular Velocity vs Time')
+plt.xlim((0, a[0]))
+plt.grid()
+
+
+plt.subplot(3,1,3)
+plt.plot(t_list, acc_list, 'k-') 
+plt.xlabel('Time (seconds)')
+plt.ylabel('Angular Acceleration (rad/s^2)')
+plt.title('Angular Acceleration vs Time')
+plt.xlim((0, a[0]))
+plt.grid()
+plt.tight_layout()
+plt.show()
+
+new.write(plot)
+fin.close()
+new.close()
 
