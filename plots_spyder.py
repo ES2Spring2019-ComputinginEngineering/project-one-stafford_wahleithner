b322c571455261083dcee546b04b13fac34c5c09
ZosiaStafford <46976875+ZosiaStafford@users.noreply.github.com>
2019-03-04 16:17:35 -0500

    Update plots_spyder.py

--- plots_spyder.py
+++ plots_spyder.py
@@ -10,7 +10,7 @@ os.chdir("C:\\Users\zosia\mu_code")
 
 filename = input("What is the filename? ")
 fin = open(filename)
-new = open("data_plots.py", 'w+')
+
 
 a = []
 ang_list = []
@@ -30,53 +30,60 @@ for line in fin:
     z = float(a[3])
     
     ang = m.atan2(x,(m.sqrt(y**2+z**2)))
-    vel = ang/t
-    acc = -9.81/(15*m.sin(ang))
-    
     ang_list.append(ang)
-    vel_list.append(vel)
-    acc_list.append(acc)
     t_list.append(t)
+    
+for i in range(len(ang_list)-1):
+    ang_diff = ang_list[i+1] - ang_list[i]
+    time_diff = t_list[i+1] - t_list[i]
+    vel = ang_diff / time_diff
+    vel_list.append(vel)
 
-print(ang_list)
-print("****")
-print(vel_list)
-print("****")
-print(acc_list)
-print("****")
-print(t_list)
+for i in range(len(vel_list)-1):
+    vel_diff = vel_list[i+1] - vel_list[i]
+    time_diff = t_list[i+1] - t_list[i]
+    acc = vel_diff / time_diff
+    acc_list.append(acc)
+    
 
-plot = plt.figure(figsize = (4,6))
+
+#Plots
+plt.figure(figsize = (4,6))
 
 plt.subplot(3,1,1)
 plt.plot(t_list, ang_list, 'r-') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Angle (rad)')
 plt.title('Angle vs Time')
-plt.xlim((0, a[0]))
+plt.xlim((0, float(a[0])))
 plt.grid()
 
-
 plt.subplot(3,1,2)
-plt.plot(t_list, vel_list, 'b-') 
+plt.plot(t_list[:-1], vel_list, 'b-') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Angular Velocity (rad/s)')
 plt.title('Angular Velocity vs Time')
-plt.xlim((0, a[0]))
+plt.xlim((0, float(a[0])))
 plt.grid()
 
-
 plt.subplot(3,1,3)
-plt.plot(t_list, acc_list, 'k-') 
+plt.plot(t_list[:-2], acc_list, 'k-') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Angular Acceleration (rad/s^2)')
 plt.title('Angular Acceleration vs Time')
-plt.xlim((0, a[0]))
+plt.xlim((0, float(a[0])))
 plt.grid()
 plt.tight_layout()
 plt.show()
 
-new.write(plot)
 fin.close()
-new.close()
 
+
+
+#period_list = []
+#for i in ang_list:
+ #   if ang_list[i] < 0 and ang_list[i+1] > 0:
+  #      p = t_list[i+1] - t_list[i]
+   #     period_list.append(p)
+        
+        
