655801e7ac053d386d9a230dc19f6130825fe068
ZosiaStafford <46976875+ZosiaStafford@users.noreply.github.com>
2019-03-06 19:47:03 -0500

    Update plots_spyder.py

--- plots_spyder.py
+++ plots_spyder.py
@@ -13,11 +13,18 @@ os.chdir("C:\\Users\zosia\mu_code")
 filename = input("What is the filename? ")
 fin = open(filename) #Get microbit data file name
 
+#start and end of significant data, used in line 72
+s = 20
+e = 177
+
 a = []
 ang_list = []
 vel_list = []
 acc_list = []
 t_list = []
+x_list = []
+y_list = []
+g = -9.81
 
 #Parsing the microbit file
 for line in fin:
@@ -35,6 +42,8 @@ for line in fin:
     #Get list of angles
     ang = m.atan2(y,(m.sqrt(x**2+z**2)))
     ang_list.append(ang)
+    x_list.append(x)
+    y_list.append(y)
     
     #Get list of velocity by differentiating
 for i in range(len(ang_list)-1):
@@ -65,18 +74,8 @@ plt.tight_layout()
 plt.show()
 fin.close()
 
-#We only need the angle graph, but these are the functions to get velocity and acceleration:
-
-#plt.subplot(3,1,2)
-#plt.plot(t_list[:-1], vel_list, 'b-') 
-#plt.xlabel('Time (seconds)')
-#plt.ylabel('Angular Velocity (rad/s)')
-#plt.title('Angular Velocity vs Time')
-#plt.xlim((0, float(a[0])))
-#plt.grid()
-
 #plt.subplot(3,1,3)
-#plt.plot(t_list[:-2], acc_list, 'k-') 
+#plt.plot(t_list[s:e], y_list[s:e], 'k-') 
 #plt.xlabel('Time (seconds)')
 #plt.ylabel('Angular Acceleration (rad/s^2)')
 #plt.title('Angular Acceleration vs Time')
@@ -84,8 +83,8 @@ fin.close()
 #plt.grid()
 
 #Cutting our graph to when the pendulum moves in harmonic motion
-time = np.array(t_list[20:285])
-y = np.array(ang_list[20:285])
+time = np.array(t_list[s:e])
+y = np.array(ang_list[s:e])
 
 # Apply median filter to both original and noisy wave
 y_filt = sig.medfilt(y, 7)
@@ -95,6 +94,7 @@ y_pks, _ = sig.find_peaks(y, prominence = 0.05)
 y_filt_pks, _ = sig.find_peaks(y_filt, prominence = 0.05)
 
 # Plot waveforms and their peaks
+plt.figure(figsize = (6,4))
 plt.subplot(2,1,1)
 plt.plot(time, y, 'r-', time[y_pks], y[y_pks], 'b.')
 plt.xlabel('Time (seconds)')
